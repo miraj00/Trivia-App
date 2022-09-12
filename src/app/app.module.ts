5c19bfa6a1e3a90629e966432cda752a851d8e24
miraj <miraj00@gmail.com>
2022-09-12 19:59:44 -0400

    components and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { HighScoresComponent } from './components/high-scores/high-scores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionsComponent,
+    HighScoresComponent
   ],
   imports: [
     BrowserModule,
