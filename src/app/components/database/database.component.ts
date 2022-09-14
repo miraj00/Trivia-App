83731c90b0babd92fa138bec794061ea21ee98f7
miraj <miraj00@gmail.com>
2022-09-13 23:36:31 -0400

    database code added

---
+++ src/app/components/database/database.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+import { scoreDetails } from 'src/app/interfaces/player';
+import { DatabaseService } from 'src/app/services/database.service';
+//   import { ProfileService } from 'src/app/services/profile.service';
+
+@Component({
+  selector: 'app-database',
+  templateUrl: './database.component.html',
+  styleUrls: ['./database.component.css']
+})
+export class DatabaseComponent implements OnInit {
+
+  database: scoreDetails[] = [];
+  loading = true;
+
+
+  constructor(private databaseService: DatabaseService, private profileService: ProfileService) { }
+
+  ngOnInit(): void {
+    this.loadDatabase();
+  }
+
+  loadDatabase() {
+    this.loading = true;
+    this.databaseService.getScores().subscribe(newDatabase => {
+      this.database = newDatabase;
+      this.loading = false;
+    });
+  }
+
+  removeScore(database: scoreDetails) {
+    this.database = [];
+    this.loading = true;
+    this.databaseService.removeScore(database.playerName).subscribe(() => {
+      this.loadDatabase();
+      // if (this.profileService.getPet() === pokemon.sprites.defaultFront) {
+      //   this.profileService.setPet(null);
+      });
+    }
+  
+
+  setPet(database: scoreDetails) {
+   //  this.profileService.setPet(pokemon.sprites.defaultFront);
+  }
+}
